224dcba4d01e98b3eaf156d0d36ddb0c8ef0c938
gsh <gsh@localhost>
2023-02-23 14:16:16 -0600

    Assignment 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,11 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.rectangle(0.5, 0.5, .4, .25);
+		StdDraw.rectangle(.25, .6, 0.15, 0.15);
+		StdDraw.rectangle(.45, .5, 0.075, 0.075);
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,55 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+	
+		String shapeType = in.nextLine();
+	
+		String rectangle = null;
+		String triangle = null;
+		String ellipse = null;
+		
+		if (shapeType == rectangle)
+		{
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.hasNext();
+			double x = in.nextInt();
+			double y = in.nextInt();
+			double halfWidth = in.nextInt();
+			double halfHeight = in.nextInt();
+		}
+		
+		if (shapeType == triangle)
+		{
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.hasNext();
+			double x = in.nextInt();
+			double y = in.nextInt();
+			String type = in.next();
+		}
+		
+		if (shapeType == ellipse)
+		{
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.hasNext();
+			double x = in.nextInt();
+			double y = in.nextInt();
+			double semiMinorAxis = in.nextInt();
+			double semiMajorAxis = in.nextInt();
+		}
+		
+		StdDraw.ellipse(.5, .5, .25, .5);
+	
+		
+		
+		
+		
+		
 	}
+	
 }
